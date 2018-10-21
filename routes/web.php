d412be5b054f60fa100e33c83455c1654e43b693
吳駿松 <jo32229hn@gmail.com>
2018-10-21 19:57:42 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,16 @@ Route::get('/', function () {
     return view('tasks');
 });
 
-Route::post('/task',function(Request$request){});
+Route::post('/task',function(Request$request){
+    $validator = Validator::make($request->all(),['name'=>'required|max:255',]);
+    if($validator->fail()){
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+});
 Route::delete('/task/{task}',function(Task$task){});
